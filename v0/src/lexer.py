4e8d47075015bca394d327d0cc8f3fcef6acf8cf
Ido Tauman <ido032009@gmail.com>
2026-02-21 19:31:39 +0200

    Added string/char handling

--- v0/src/lexer.py
+++ v0/src/lexer.py
@@ -1,4 +1,5 @@
 from .tokens import *
+from .util import error
 
 def isValidEndLine(t: Token) -> bool:
     literal_types = (IntLiteral, CharLiteral, FloatLiteral, StringLiteral, Identifier)
@@ -17,6 +18,9 @@ def lexer(input_path: str) -> list[Token]:
 
     open_counts = {"paren": 0, "bracket": 0, "brace": 0}
 
+    open_string = False
+    open_char = False
+
     PARENTHESIS = {
         '(': (OpenParenthesis, "paren", 1),
         ')': (CloseParenthesis, "paren", -1),
@@ -26,11 +30,104 @@ def lexer(input_path: str) -> list[Token]:
         '}': (CloseBrace, "brace", -1),
     }
 
+    buffer = ''
+
     with open(input_path, 'r', encoding='utf-8') as f:
         for line in f:
             i = 0
             while i < len(line):
                 char: str = line[i]
+
+                if (open_string):
+                    if char == '\n':
+                        error("Unterminated string literal")
+
+                    if char == '"':
+                        tokens.append(StringLiteral(buffer))
+                        buffer = ''
+                        i += 1
+                        open_string = False
+                        continue
+
+                    if char == '\\':
+                        if i + 1 >= len(line):
+                            error("Trailing backslash at the end of line")
+
+                        next = line[i + 1]
+                        if next == '\'':
+                            buffer += '\''
+                        elif next == '\"':
+                            buffer += '\"'
+                        elif next == '\\':
+                            buffer += '\\'
+                        elif next == 'א':
+                            buffer += '\a'
+                        elif next == 'ב':
+                            buffer += '\b'
+                        elif next == 'נ':
+                            buffer += '\n'
+                        elif next == 'ר':
+                            buffer += '\r'
+                        elif next == 'ט':
+                            buffer += '\t'
+                        else:
+                            error(f"Invalid escape sequence: \\{next}")
+                        i += 2
+                        continue
+
+                    buffer += char
+                    i += 1
+                    continue
+
+
+                if (open_char):
+                    if char == '\n':
+                        error("Unterminated char literal")
+
+                    if char == "'":
+                        if len(buffer) == 0:
+                            error("Empty char literal")
+                        tokens.append(CharLiteral(buffer))
+                        buffer = ''
+                        i += 1
+                        open_char = False
+                        continue
+
+                    if char == '\\':
+                        if i + 1 >= len(line):
+                            error("Trailing backslash at the end of line")
+
+                        next = line[i + 1]
+                        if next == '\'':
+                            buffer += '\''
+                        elif next == '\"':
+                            buffer += '\"'
+                        elif next == '\\':
+                            buffer += '\\'
+                        elif next == 'א':
+                            buffer += '\a'
+                        elif next == 'ב':
+                            buffer += '\b'
+                        elif next == 'נ':
+                            buffer += '\n'
+                        elif next == 'ר':
+                            buffer += '\r'
+                        elif next == 'ט':
+                            buffer += '\t'
+                        else:
+                            error(f"Invalid escape sequence: \\{next}")
+                        i += 2
+                        if len(buffer) > 1:
+                            error("Char literal longer than one character")
+                        continue
+
+                    buffer += char
+                    i += 1
+
+                    if len(buffer) > 1:
+                        error("Char literal longer than one character")
+                    continue
+
 
                 if char.isspace():
                     if char == '\n':
@@ -44,6 +141,15 @@ def lexer(input_path: str) -> list[Token]:
                     cclass, key, delta = PARENTHESIS[char]
                     tokens.append(cclass())
                     open_counts[key] += delta
+                    i += 1
+                    continue
+
+                if char == '"':
+                    open_string = True
+                    i += 1
+                    continue
+                elif char == "'":
+                    open_char = True
                     i += 1
                     continue
 
